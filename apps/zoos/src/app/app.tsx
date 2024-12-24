e3ae7968cd654417eed631d43b8ff183f3a945e8
Alex <art@zoohilldata.com>
2024-12-24 01:57:49 -0600

    small tweaks to get to minimal monorepo (no packages)
    - vscode settings.json
    - team animal svg's to experiment with `src/assets`
    - background color

--- apps/zoos/src/app/app.tsx
+++ apps/zoos/src/app/app.tsx
@@ -1,9 +1,15 @@
-import NxWelcome from './nx-welcome';
+import alex from "../assets/alex-snail.svg";
+import bryce from "../assets/bryce-dolphin.svg";
+import borst from "../assets/borst-turtle.svg";
 
 export function App() {
   return (
-    <div>
-      <NxWelcome title="zoos" />
+    <div className="bg-background text-foreground flex h-screen w-screen flex-col items-center justify-center gap-2">
+      <div className="flex w-full flex-wrap items-center justify-around">
+        <img src={borst} alt="Borst Turtle" className="h-96 w-96" />
+        <img src={alex} alt="Alex Snail" className="h-96 w-96" />
+        <img src={bryce} alt="Bryce Dolphin" className="h-96 w-96" />
+      </div>
     </div>
   );
 }
--- apps/zoos/tailwind.config.js
+++ apps/zoos/tailwind.config.js
@@ -1,17 +1,22 @@
-const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
-const { join } = require('path');
+const { createGlobPatternsForDependencies } = require("@nx/react/tailwind");
+const { join } = require("path");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     join(
       __dirname,
-      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+      "{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}",
     ),
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
   },
   plugins: [],
 };
