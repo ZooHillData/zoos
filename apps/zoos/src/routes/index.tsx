244a3013029a4902ba9b34f428bb019426436728
Alex <art@zoohilldata.com>
2024-12-27 12:45:09 -0600

    Update zoos homescreen

--- apps/zoos/src/routes/index.tsx
+++ apps/zoos/src/routes/index.tsx
@@ -19,6 +19,8 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
+  buttonVariants,
+  cn,
 } from "@zoos/ui-shad";
 
 import alex from "../assets/alex-snail.svg";
@@ -29,6 +31,23 @@ function RouteComponent() {
   const [open, setOpen] = React.useState(false);
   return (
     <div className="bg-background text-foreground flex h-screen w-screen flex-col items-center justify-center gap-2">
+      <h1 className="text-6xl font-bold">Zoos</h1>
+      <h2 className="text-2xl font-semibold">The "ShadCN" of Tanstack</h2>
+      <div className="flex items-center">
+        <a
+          className={cn(buttonVariants({ variant: "link" }), "h-fit py-0")}
+          href="https://github.com/ZooHillData/zoos/discussions/new?category=ideas"
+        >
+          Ideas, Feedback, Bugs
+        </a>
+        <div className="h-full border" />
+        <a
+          className={cn(buttonVariants({ variant: "link" }), "h-fit py-0")}
+          href="https://www.npmjs.com/~alexryanterry"
+        >
+          NPM Packages
+        </a>
+      </div>
       <div className="flex w-full flex-wrap items-center justify-around">
         <img src={borst} alt="Borst Turtle" className="h-96 w-96" />
         <img src={alex} alt="Alex Snail" className="h-96 w-96" />
