d03b79ae532e16fcf50eba6b40d8891ae5be2519
Alex <art@zoohilldata.com>
2024-12-28 15:45:52 -0500

    chore(zoos): add brian and redo homepage layout [skip ci]

--- apps/zoos/src/routes/index.tsx
+++ apps/zoos/src/routes/index.tsx
@@ -26,49 +26,72 @@ import {
 import alex from "../assets/alex-snail.svg";
 import bryce from "../assets/bryce-dolphin.svg";
 import borst from "../assets/borst-turtle.svg";
+import brian from "../assets/brian-monkey.svg";
 
-function RouteComponent() {
+const Icons = () => (
+  <div className="mx-auto flex w-fit flex-wrap items-center gap-x-8">
+    <img src={alex} alt="Alex Snail" className="h-24 w-24" />
+    <img src={bryce} alt="Bryce Dolphin" className="h-24 w-24" />
+    <img src={brian} alt="Brian Monkey" className="h-24 w-24" />
+    <img src={borst} alt="Borst Turtle" className="h-24 w-24" />
+  </div>
+);
+
+const GetStartedButton = () => {
   const [open, setOpen] = React.useState(false);
   return (
-    <div className="bg-background text-foreground flex h-screen w-screen flex-col items-center justify-center gap-2">
-      <h1 className="text-6xl font-bold">Zoos</h1>
-      <h2 className="text-2xl font-semibold">The "ShadCN" of Tanstack</h2>
-      <div className="flex items-center">
-        <a
-          className={cn(buttonVariants({ variant: "link" }), "h-fit py-0")}
-          href="https://github.com/ZooHillData/zoos/discussions/new?category=ideas"
-        >
-          Ideas, Feedback, Bugs
-        </a>
-        <div className="h-full border" />
-        <a
-          className={cn(buttonVariants({ variant: "link" }), "h-fit py-0")}
-          href="https://www.npmjs.com/~alexryanterry"
-        >
-          NPM Packages
-        </a>
+    <Dialog open={open} onOpenChange={(open) => setOpen(open)}>
+      <DialogTrigger asChild>
+        <Button>Get Started</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Thank you for your patience</DialogTitle>
+          <DialogDescription>
+            We are still working on this. Please check back soon!
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <Button onClick={() => setOpen(false)}>Ok</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+const Links = () => (
+  <div className="flex items-center">
+    <a
+      className={cn(buttonVariants({ variant: "link" }), "h-fit py-0")}
+      href="https://github.com/ZooHillData/zoos/discussions/new?category=ideas"
+    >
+      Ideas, Feedback, Bugs
+    </a>
+    <div className="h-full border" />
+    <a
+      className={cn(buttonVariants({ variant: "link" }), "h-fit py-0")}
+      href="https://www.npmjs.com/~alexryanterry"
+    >
+      NPM Packages
+    </a>
+  </div>
+);
+
+function RouteComponent() {
+  return (
+    <div className="bg-background text-foreground flex h-screen w-screen flex-col items-center gap-8 overflow-auto">
+      <div className="mt-auto flex flex-col items-center gap-4">
+        <h1 className="text-6xl font-bold">Zoos</h1>
+        <Links />
+        <p className="max-w-[400px] text-balance text-center text-sm italic">
+          Simple, composable React libraries to help take advantage of our top
+          Tanstack, ShadCN, Zustand, Supabase patterns.
+        </p>
+        <GetStartedButton />
       </div>
-      <div className="flex w-full flex-wrap items-center justify-around">
-        <img src={borst} alt="Borst Turtle" className="h-96 w-96" />
-        <img src={alex} alt="Alex Snail" className="h-96 w-96" />
-        <img src={bryce} alt="Bryce Dolphin" className="h-96 w-96" />
+      <div className="mt-auto">
+        <Icons />
       </div>
-      <Dialog open={open} onOpenChange={setOpen}>
-        <DialogTrigger asChild>
-          <Button>Get Started</Button>
-        </DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Thank you for your patience</DialogTitle>
-            <DialogDescription>
-              We are still working on this. Please check back soon!
-            </DialogDescription>
-          </DialogHeader>
-          <DialogFooter>
-            <Button onClick={() => setOpen(false)}>Ok</Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
     </div>
   );
 }
