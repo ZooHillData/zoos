1989a8c163c38291ca872a1cd1497c4edf92e40a
Alex <art@zoohilldata.com>
2025-02-18 15:39:41 -0700

    zoos: update combobox-checkbox-group route to show multi/single example

--- apps/zoos/src/routes/core/react-form/combobox-checkbox-group.tsx
+++ apps/zoos/src/routes/core/react-form/combobox-checkbox-group.tsx
@@ -8,56 +8,12 @@ export const Route = createFileRoute(
 
 import React from "react";
 
-import { Input, Label, createCn } from "@zoos/shadcn";
-import { CheckboxGroupVirtual, useVirtualCombobox } from "@zoos/react-form";
+import { Label } from "@zoos/shadcn";
+
+import { CheckboxWithLabel, ComboboxCheckboxGroup } from "@zoos/react-form";
 import { createData, createRandomUser } from "../../../community/fake-data";
 
-const ComboboxCheckboxGroup = ({
-  options,
-  inputClassName,
-  ...props
-}: Omit<React.ComponentProps<typeof CheckboxGroupVirtual>, "virtualizer"> & {
-  inputClassName?: string;
-}) => {
-  const {
-    query,
-    setQuery,
-    optionsFiltered,
-    hasUniqueValues,
-    virtualizer,
-    scrollRef,
-  } = useVirtualCombobox({
-    options,
-    virtualizerOptions: { estimateSize: () => 24 },
-  });
-  if (!hasUniqueValues) {
-    window.alert(
-      "Options must have unique values. That's one benefit of `Options` as an object with IDs.",
-    );
-  }
-
-  return (
-    <div className="h-full space-y-3">
-      <Input
-        className={inputClassName}
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      {/* <div className="flex max-h-[200px] flex-col gap-3 overflow-auto"> */}
-      <div className="h-full overflow-auto" ref={scrollRef}>
-        <CheckboxGroupVirtual
-          options={optionsFiltered}
-          virtualizer={virtualizer}
-          className={createCn("flex flex-col gap-3")}
-          {...props}
-        />
-      </div>
-      {/* </div> */}
-    </div>
-  );
-};
-
-function RouteComponent() {
+const SingleSelect = () => {
   const [value, setValue] = React.useState<string[]>([]);
 
   const options = React.useMemo(
@@ -72,14 +28,83 @@ function RouteComponent() {
   );
 
   return (
-    <div className="h-screen space-y-3 overflow-hidden p-32">
-      <Label>Combobox with checkboxes (virtualized)</Label>
-      <ComboboxCheckboxGroup
-        inputClassName="w-[300px]"
-        options={options}
-        value={value}
-        onChange={setValue}
-      />
+    <ComboboxCheckboxGroup
+      inputClassName="w-[300px]"
+      options={options}
+      value={value}
+      // Single select set using the option
+      onChange={(value, option) =>
+        // If value is same, clear the value
+        // Otherwise, set the value to the last option clicked (single select)
+        setValue((prev) => (prev[0] === option.value ? [] : [option.value]))
+      }
+      inputPlaceHolder={
+        options
+          .filter((option) => value.includes(option.value))
+          .map((option) => option.label)
+          .join(",") || "Select an option"
+      }
+    />
+  );
+};
+
+const MultiSelect = () => {
+  const [value, setValue] = React.useState<string[]>([]);
+
+  const options = React.useMemo(
+    () =>
+      createData(createRandomUser, { count: 10000 })
+        .map((user) => ({
+          value: user.user_id,
+          label: `${user.first_name} ${user.last_name}`,
+        }))
+        .sort((a, b) => a.label.localeCompare(b.label)),
+    [],
+  );
+
+  const inputPlaceholder = React.useMemo(() => {
+    const filteredOptions = options.filter((option) =>
+      value.includes(option.value),
+    );
+    if (filteredOptions.length > 2) {
+      return `${filteredOptions[0].label}, ${filteredOptions[1].label}, ${filteredOptions.length - 2} more`;
+    } else if (filteredOptions.length === 0) {
+      return "Select option(s)";
+    } else {
+      return filteredOptions.map((option) => option.label).join(", ");
+    }
+  }, [options, value]);
+
+  return (
+    <ComboboxCheckboxGroup
+      options={options}
+      value={value}
+      onChange={setValue}
+      inputClassName="w-[300px]"
+      inputPlaceHolder={inputPlaceholder}
+    />
+  );
+};
+
+function RouteComponent() {
+  const [isSingleSelect, setIsSingleSelect] = React.useState(true);
+
+  return (
+    <div className="h-screen space-y-8 overflow-hidden p-32">
+      <div className="space-y-1">
+        <CheckboxWithLabel
+          label="Multi select?"
+          checked={!isSingleSelect}
+          onCheckedChange={() => setIsSingleSelect((prev) => !prev)}
+        />
+        <p className="text-muted-foreground text-sm italic">
+          virtualized, {isSingleSelect ? "single" : "multi"} select
+        </p>
+      </div>
+      <div className="space-y-1">
+        <Label>Combobox with checkboxes</Label>
+        {isSingleSelect ? <SingleSelect /> : <MultiSelect />}
+      </div>
     </div>
   );
 }
